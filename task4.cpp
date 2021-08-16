653d85f2d61fa208b78b5568b277aa0f9bbc3d47
Aftab Ahmed <88955526+Aftab1311@users.noreply.github.com>
2021-08-16 08:44:25 +0530

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -1,3 +1,20 @@
+// TASK 4
+// Name: Aftab Ahmed
+/* AIM: Given an integer N, the task is to count the number of ways so that N can be written as the sum of a prime number and twice of a square, 
+        i.e. N = 2*A2 + P, where P can be any prime number and A is any positive integer.
+    Input: N = 9
+    Output: 1
+    Explanation:
+        9 can be represented as sum of prime number and twice a square in only one way –
+        N = 9 = 7 + 2*(1^2)
+    Input: N = 15
+    Output: 2
+    Explanation:
+        15 can be represented as sum of prime number and twice a square in two ways –
+        N = 15 = 7 + 2 * (2^2)
+        N = 15 = 13 + 2 * (1^2)
+*/
+
 #include <iostream>
 using namespace std;
 
